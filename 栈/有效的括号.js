ed49a2d0929c56d9cd65b162902fa89e026f7e9c
dane <2401757537@qq.com>
2022-09-15 09:52:20 +0800

    feature: 有效的括号-t0

--- 栈/有效的括号.js
+++ 栈/有效的括号.js
@@ -1,1 +1,54 @@
-console.log('test==')
+let str1 = '([]{})'
+let str2 = '([{])}])'
+
+
+/**
+ * 时间复杂度O(n)
+ * 空间复杂度O(n)
+ * @param {*} str 
+ * @returns 
+ * 推演
+ * ([]{})
+ * 1. stack: ['(']
+ * 2. stack: ['(', '[']
+ * 3. end = '['    newStart = ]    =>  stack.pop => stack = ['(']
+ * 4. stack: ['(', '{']
+ * 5. end = '{'  newStart = }  => stack.pop => stack = ['(']
+ * 
+ * ([{])}
+ * 1. stack: ['(']
+ * 2. stack: ['(', '[']
+ * 3. stack: ['(', '[', '{']
+ * 4. end = '{' newStart = ']' retrun false
+ * 6. end = '{' newStart = ')' retrun false
+ * 6. end = '{' newStart = '}' => stack.pop => stack = ['(', '[']
+ */
+const fun1 = (str) => {
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const start = str[i]
+    const leftList = ['(', '{', '[']
+    const rightList = [')', '}', ']']
+    // 如果匹配条件则将其入栈
+    if (leftList.includes(start)) {
+      stack.push(start)
+    } else {
+      // 获取最后一个入栈的
+      const end = stack[stack.length - 1]
+      const index = leftList.findIndex(item => item === end)
+      const rightIndex = rightList.findIndex(item => item === start)
+      if (index < 0) {
+        return false
+      }
+      if (index === rightIndex) {
+        stack.pop()
+      } else {
+        return false
+        // continue
+      }
+    }
+  }
+  return stack.length === 0
+}
+
+console.log('result', str2, fun1(str2));
